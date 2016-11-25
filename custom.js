65a4777aa050b5d4a3b699cd51ec4fc07c165b35
webvn20 <phungquocdong@gmail.com>
2016-11-25 21:55:55 +0700

    Add files via upload

---
+++ custom.js
@@ -0,0 +1,15 @@
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-55451023-1', 'auto');
+  ga('send', 'pageview');
+
+
+
+$(".typical-products").owlCarousel({autoPlay:2000,items : 1});
+$('.detail ul.function').append('<li class="HL">Hotline: 0168.408.9094</li>')
+
+
+(function(d,s,id){var z=d.createElement(s);z.type="text/javascript";z.id=id;z.async=true;z.src="//static.zotabox.com/e/6/e68c77a138d3c55d1edc1d60938adf3e/widgets.js";var sz=d.getElementsByTagName(s)[0];sz.parentNode.insertBefore(z,sz)}(document,"script","zb-embed-code"));
